40fa86fcde49fcc58c1e215f6f50939413a8a369
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2025-12-04 21:29:45 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -87,4 +87,8 @@ console.log(
   app._router.stack.map((r) => r.route?.path).filter(Boolean)
 );
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+  console.log(`Server running`);
+});
+
