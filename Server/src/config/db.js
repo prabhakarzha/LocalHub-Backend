9788178ae6c853eae89706bfdeee1132610f64e2
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2026-02-22 13:51:14 +0530

    Update db.js

--- Server/src/config/db.js
+++ Server/src/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Successfully Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Database Connection Error: ${error.message}`);
